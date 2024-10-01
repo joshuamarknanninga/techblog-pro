493b4ac352720cf6187b5f5fdea71ff15014458c
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-01 14:14:08 -0500

    fixed sequalize login at index.js

--- models/index.js
+++ models/index.js
@@ -6,9 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.tech_blog_db,
+  process.env.joshuamarknanninga,
+  process.env['12345'],
   {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
